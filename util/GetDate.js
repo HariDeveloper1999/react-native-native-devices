7e8e7764805313e2cb321d4272694cc54aff64d8
haribabu <31000077@carandbike.com>
2023-10-28 06:52:24 +0530

    removing unused

--- util/GetDate.js
+++
@@ -1,8 +0,0 @@
-export function GetFullDate(date){
-    return date.toISOString().slice(0,10)
-
-}
-
-export function getDateMinusdate(date,days){
-    return new Date(date.getFullYear(),date.getMonth(),date.getDate()-days)
-}
